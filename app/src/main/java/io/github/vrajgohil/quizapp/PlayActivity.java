c0dfb06b349ed6e41c4eecb1b4f2eb852d300d7d
Vraj Gohil <40790016+vraj07@users.noreply.github.com>
2019-04-10 13:09:21 +0530

    Added firebase support.

--- app/src/main/java/io/github/vrajgohil/quizapp/PlayActivity.java
+++ app/src/main/java/io/github/vrajgohil/quizapp/PlayActivity.java
@@ -2,6 +2,8 @@ package io.github.vrajgohil.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+        ArrayList<Questions> question = new ArrayList<Questions>();
+        TextView questionView = (TextView) findViewById(R.id.questionView);
+        questionView.setText("Hello");
     }
 }
---
+++ app/src/main/java/io/github/vrajgohil/quizapp/Questions.java
@@ -0,0 +1,19 @@
+package io.github.vrajgohil.quizapp;
+
+class Questions {
+        String question;
+        String option1;
+        String option2;
+        String option3;
+        String option4;
+        int answer;
+        Questions(String question,String option1,String option2,String option3,String option4,int answer){
+            this.question=question;
+            this.option1=option1;
+            this.option2=option2;
+            this.option3=option3;
+            this.option4=option4;
+            this.answer=answer;
+        }
+
+}
